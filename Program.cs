6158c912f6a7dbc13903ca2c7506c5967c498aa7
Moonyll <s.pouwel@laposte.net>
2018-12-19 11:22:03 +0100

    Bex6

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        string[] months = new string[] {"Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre"};
+            
+            foreach (string month in months)
+            {
+             Console.WriteLine($"{month}");
+            }
+        }
+    }
+}
